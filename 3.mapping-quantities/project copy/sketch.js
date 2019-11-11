26ea3b3086fb8d44c571f97c1c6f47002ca237ee
jjung11111 <55676631+jjung11111@users.noreply.github.com>
2019-11-11 15:53:34 -0500

    a

--- 3.mapping-quantities/project copy/sketch.js
+++ 3.mapping-quantities/project copy/sketch.js
@@ -1,11 +1,33 @@
 var data
 
 function preload(){
-  data = loadTable('data/totals.csv', 'csv', 'header')
+  data = loadTable('data/nuclear explosions - Sheet1.csv', 'csv', 'header')
 }
 
 function setup(){
   createCanvas(800, 600)
+  var table = data
 
-  print('csv data converted to a p5 "Table" object:', data)
+  // log the whole dataset to the console so we can poke around in it
+  print(table)
+
+  // set up typography
+  textFont("Rokkitt")
+  textSize(16)
+  fill(30)
+  noStroke()
+
+  var x = 200
+  var y = 100
+  var rowHeight = 60
+  var colWidth = 40
+
+  textStyle(BOLD)
+  textAlign(RIGHT)
+  for (var c=1; c<table.getColumnCount(); c++){
+    text(table.columns[c], x-colWidth, y)
+    y += rowHeight
+  }
+
+
 }
